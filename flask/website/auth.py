458506325059df24c02f2d783bf418fc5d0fea48
robertmonopoly <124234434+robertmonopoly@users.noreply.github.com>
2023-06-07 16:27:01 -0500

    starting website

---
+++ flask/website/auth.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+from sqlalchemy.exc import IntegrityError
+from flask import session
+
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Invalid credentials, try again.', category='error')
+        else:
+            flash('Invalid credentials', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        try:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+                password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            session['first_name'] = first_name  # Store the first name in the session
+            return redirect(url_for('views.home'))
+        except IntegrityError:
+            flash('That email is already in use.', category='error')
+        except ValueError:
+            flash('Invalid credentials', category='error')
+
+    return render_template("sign_up.html", user=current_user)
