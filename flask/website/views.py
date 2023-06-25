77cba07cbe40c53d44c0bb85bef5a465e7778729
robertwendy2020 <124234434+robertwendy2020@users.noreply.github.com>
2023-06-25 15:32:28 -0500

    fixed navbar

--- flask/website/views.py
+++ flask/website/views.py
@@ -9,33 +9,16 @@ import datetime
 
 views = Blueprint('views', __name__)
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/', methods=['GET'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+    if current_user.is_authenticated:
+        profile = ProfileData.query.filter_by(id=current_user.id).first()
+    else:
+        profile = None
 
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
+    return render_template("home.html", user=current_user, profile=profile)
 
-    return render_template("home.html", user=current_user)
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
 
 @views.route('/complete_profile', methods=['GET', 'POST'])
 @login_required
@@ -77,28 +60,30 @@ def complete_profile():
 @views.route('/view_history', methods=['GET'])
 @login_required
 def view_history():
+    profile = ProfileData.query.filter_by(id=current_user.id).first()
     orders = FuelOrderFormData.query.filter_by(user_id=current_user.id).all()
-    return render_template("view_history.html", user=current_user, orders = orders)
+    return render_template("view_history.html", user=current_user, profile=profile, orders=orders)
+
 
 @views.route('/fuel_price_form', methods=['GET', 'POST'])
 @login_required
 def fuel_price_form():
+    profile = ProfileData.query.filter_by(id=current_user.id).first()
     price = None  # Initialize the price variable
 
     if request.method == 'POST':
         gallons = float(request.form['gallons_requested'])  # Convert to float
         delivery_date = request.form['delivery_date']
-        
+
         delivery_date = datetime.datetime.strptime(delivery_date, '%Y-%m-%d').date()
-        profile_data = ProfileData.query.filter_by(id=current_user.id).first()
-        # check if the customer has ordered fuel before
-        is_new_customer = db.session.query(FuelOrderFormData.query.filter(FuelOrderFormData.user_id ==current_user.id).exists()).scalar()
         
-        if profile_data.in_state_status:
+        is_new_customer = FuelOrderFormData.query.filter_by(user_id=current_user.id).first() is None
+
+        if profile.in_state_status:
             location_fee = 5
         else:
             location_fee = 15
-        
+
         if is_new_customer:
             discount_rate = 0
         else:
@@ -111,34 +96,36 @@ def fuel_price_form():
         elif company_profit_margin >= 1:
             profit_fee = 0.50
         else:
-            profit_fee = 2.00            
+            profit_fee = 2.00
 
         price = (3.36 * gallons) + (gallons * location_fee) + (gallons * discount_rate) + (gallons * profit_fee)
         price_per_gallon = 3.36 + location_fee + discount_rate + profit_fee
-        print(price)
 
         fuel_order_form_data = FuelOrderFormData(
             gallons=gallons,
             delivery_date=delivery_date,
-            address_1=profile_data.address_1,
-            address_2=profile_data.address_2,
-            city=profile_data.city,
-            state=profile_data.state,
-            zip_code=profile_data.zip_code,
+            address_1=profile.address_1,
+            address_2=profile.address_2,
+            city=profile.city,
+            state=profile.state,
+            zip_code=profile.zip_code,
             price=price,
-            user=current_user  # Assign the current user to the user attribute
+            user=current_user
         )
-        
+
         db.session.add(fuel_order_form_data)
         db.session.commit()
-        
+
         flash("Fuel ordered successfully!")
 
-        return render_template("fuel_price_form.html", user=current_user, price=price, address_1=profile_data.address_1, price_per_gallon=price_per_gallon)
-    
-    return render_template("fuel_price_form.html", user=current_user, price=price)
+        return render_template(
+            "fuel_price_form.html",
+            user=current_user,
+            profile=profile,
+            price=price,
+            address_1=profile.address_1,
+            price_per_gallon=price_per_gallon
+        )
 
-
-
-
+    return render_template("fuel_price_form.html", user=current_user, profile=profile, price=price)
 
