18abc09732a796191f9b03c25df62d74b2c3541f
Slendy <josh@slendy.pw>
2023-07-03 03:36:17 -0500

    Add unit tests, coverage, and instructions

--- README.md
+++ README.md
@@ -3,16 +3,28 @@
 ## Running
 
 Assuming you have Python 3.3 or later installed you should create a virtual environment using the `venv` command.
-
-`python3 -m venv .venv`
+```
+python3 -m venv .venv
+```
 
 Then, dependencies must be installed and this can be done using pip and the requirements.txt file
-
-`pip install -r flask/requirements.txt`
+```
+pip install -r flask/requirements.txt
+```
 
 You can run the project by running the main.py file
-
-`python3 main.py`
+```
+python3 main.py
+```
 
 ## Running Tests
-TODO
+
+Unit tests are found in the `/tests` folder and can be run by executing the `tests.py` file
+```
+python tests.py
+```
+
+Coverage reports can be generated like so:
+```
+coverage run tests.py && coverage report
+```
---
+++ flask/tests/test.py
@@ -0,0 +1,116 @@
+import unittest
+from flask_testing import TestCase
+from datetime import datetime
+
+# I hate python
+import sys
+sys.path.append('../')
+
+# Used to modify db name
+import website
+
+from website import create_app
+from website import db
+from website.models import User
+from website.models import FuelOrderFormData
+from website.models import ProfileData
+
+testHash='sha256$tUTxRU3xmVxJzNWE$fee1d05e6f009bb4c4e3aaa5adaeb49ef2b37ce5f7090a62301c97b2fb0b1b21'
+
+class WebsiteTests(TestCase):
+    def create_app(self):
+        website.DB_NAME = "testing.db"
+        return create_app()
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_login_when_user_doesnt_exist(self):
+        with self.client:
+            loginResponse = self.client.post('login', data={'username': 'test', 'password': 'test'})
+            self.assert200(loginResponse)
+            self.assert_message_flashed('Invalid credentials', category='error')
+            self.assert_template_used('login.html')
+
+    def test_login_when_credentials_invalid(self):
+        with self.client:
+            db.session.add(User(id = 1, username='test', password=testHash))
+            db.session.commit()
+            loginResponse = self.client.post('login', data={'username': 'test', 'password': 'test2'})
+            self.assert200(loginResponse)
+            self.assert_message_flashed('Invalid credentials, try again.', category='error')
+            self.assert_template_used('login.html')
+
+    def test_login_when_credentials_valid(self):
+        with self.client:
+            db.session.add(User(id = 1, username='test', password=testHash))
+            db.session.commit()
+            loginResponse = self.client.post('login', data={'username': 'test', 'password': 'test'}, follow_redirects=True)
+            self.assert_status(loginResponse, 200)
+            self.assert_template_used('home.html')
+            self.assert_context('user', User.query.filter_by(id=1).first())
+
+    def test_home_when_profile_data_not_present(self):
+        with self.client:
+            db.session.add(User(id = 1, username='test', password=testHash))
+            db.session.commit()
+            self.client.post('login', data={'username': 'test', 'password': 'test'})
+            response = self.client.get('/')
+            self.assert_status(response, 200)
+            self.assert_template_used('home.html')
+            self.assert_context('user', User.query.filter_by(id=1).first())
+            self.assert_context('profile', None)
+
+    def test_home_when_profile_data_present(self):
+        with self.client:
+            db.session.add(User(id = 1, username='test', password=testHash))
+            db.session.add(ProfileData(id=1))
+            db.session.commit()
+            self.client.post('login', data={'username': 'test', 'password': 'test'})
+            response = self.client.get('/')
+            self.assert_status(response, 200)
+            self.assert_template_used('home.html')
+            self.assert_context('user', User.query.filter_by(id=1).first())
+            self.assert_context('profile', ProfileData.query.filter_by(id=1).first())
+
+    def test_history(self):
+        with self.client:
+            db.session.add(User(id = 1, username='test', password=testHash))
+            db.session.add(FuelOrderFormData(id=1,
+                                             user_id = 1,
+                                             gallons=1,
+                                             delivery_date=datetime(2023, 1, 1),
+                                             address_1='test',
+                                             address_2='test',
+                                             city='test',
+                                             state='TX',
+                                             zip_code='12345',
+                                             in_state_status=True,
+                                             price=1.0))
+            db.session.add(ProfileData(id=1,
+                                        full_name='test',
+                                        address_1='test',
+                                        address_2='test',
+                                        city='test',
+                                        state='TX',
+                                        in_state_status=True,
+                                        zip_code='12345',
+                                        profile_completed=True))
+            db.session.commit()
+
+            loginResponse = self.client.post('/login', data={'username': 'test', 'password': 'test'})
+            self.assert_status(loginResponse, 302)
+            
+            response = self.client.get('/view_history')
+
+            self.assert200(response)
+            self.assert_template_used('view_history.html')
+            self.assert_context('orders', FuelOrderFormData.query.filter_by(user_id=1).all())
+            self.assert_context('profile', ProfileData.query.filter_by(id=1).first())
+
+if __name__ == '__main__':
+    unittest.main()
